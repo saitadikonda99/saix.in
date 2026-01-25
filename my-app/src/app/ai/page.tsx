b616c0431563d4515045b20c0fe7becd32517cb1
saitadikonda99 <saitadikonda.tech@gmail.com>
2026-01-25 22:10:57 +0530

    feat: add AI navbar UI

--- my-app/src/app/ai/page.tsx
+++ my-app/src/app/ai/page.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
+import Navbar from '@/components/ai/navbar/navbar'
+
 const page = () => {
   return (
-    <div>page</div>
+    <div className="w-full bg-[#1a1a1a] text-white py-2">
+        <div className="w-full sm:w-[80%] md:w-[60%] lg:w-[50%] xl:w-[40%] mx-auto py-2 sm:py-4 px-4 sm:px-0 flex flex-col items-start justify-center gap-15">
+            <Navbar />
+        </div>
+    </div>
   )
 }
 
---
+++ my-app/src/components/ai/navbar/navbar.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+        <div className="w-full border-r-1 border-l-1 border-border-ai px-4 py-1">
+            <div className="">
+                <p>SAI TADIKONDA</p>
+            </div>
+            <div className="pt-2 flex flex-col items-start justify-start">
+                <p>Navigation Links</p>
+                <p>[skills](https://saix.in/#skills)</p>
+                <p>[projects](https://saix.in/#projects)</p>
+                <p>[blogs](https://saix.in/#blogs)</p>
+                <button className="bg-white text-black my-1">[social](https://saix.in/#social)</button>
+                <button className="bg-white text-black my-1">[resume](https://saix.in/#resume)</button>
+            </div>
+        </div>
+  )
+}
+
+export default navbar
