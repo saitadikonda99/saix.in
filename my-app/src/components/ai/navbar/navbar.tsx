b0d602afcf840fa7c94516a663d2a2bbd548e4c3
saitadikonda99 <saitadikonda.tech@gmail.com>
2026-01-26 14:26:51 +0530

    fix: adjust resume link styling in navbar component

--- my-app/src/components/ai/navbar/navbar.tsx
+++ my-app/src/components/ai/navbar/navbar.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const navbar = () => {
-  return (
+    return (
         <div className="w-full bg-[#1a1a1a] sticky top-0 z-50 border-r-1 border-l-1 border-border-ai px-4 py-1">
             <div className="">
                 <p>SAI TADIKONDA</p>
@@ -12,10 +12,12 @@ const navbar = () => {
                 <p>[projects](https://saix.in/#projects)</p>
                 <p>[blogs](https://saix.in/#blogs)</p>
                 <p className="w-full bg-white text-black my-1">[social](https://saix.in/#social)</p>
-                <p className="w-full bg-white text-black my-1">[resume](https://drive.google.com/file/d/1e2psMnHHS8t_yZhb_3WZc-flHCraXOga)</p>
+                <p className="w-full bg-white text-black my-1 break-all">
+                    [resume](https://drive.google.com/file/d/1e2psMnHHS8t_yZhb_3WZc-flHCraXOga)
+                </p>
             </div>
         </div>
-  )
+    )
 }
 
 export default navbar
